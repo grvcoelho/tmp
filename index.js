605264a29787ae93a1a7be6c7085c578503241bc
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2017-07-07 14:00:59 -0300

    handler

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const cep = require('cep-promise')
 
 exports.handler = (event, context, callback) => {
   return Promise.resolve(01329010)
+    .then(cep)
     .then(result => callback(null, result))
 }
